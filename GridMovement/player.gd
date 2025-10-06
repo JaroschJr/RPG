99fd883eb9d5bc8e509c46dee6dd5dccedcb6bad
JaroschJr <jaroschjr@gmail.com>
2025-10-05 22:51:23 -0500

    Added files from a maptile and grid movement tutorial, which will be used as a template for the non-battle sections.

---
+++ GridMovement/player.gd
@@ -0,0 +1,43 @@
+extends Area2D
+
+var animation_speed = 5
+var moving = false
+var tile_size = 64
+var inputs = {
+	"right": Vector2.RIGHT,
+	"left": Vector2.LEFT,
+	"up": Vector2.UP,
+	"down": Vector2.DOWN
+}
+
+@onready var ray = $RayCast2d
+
+func _process(delta):
+	if moving:
+		return
+	for dir in inputs.keys():
+		if Input.is_action_pressed(dir):
+			move(dir)
+
+func _ready():
+	position = position.snapped(Vector2.ONE * tile_size)
+	position += Vector2.ONE * tile_size / 2
+	
+#func _unhandled_input(event):
+#	if moving:
+#		return
+#	for dir in inputs.keys():
+#		if event.is_action_pressed(dir):
+#			move(dir)
+			
+func move(dir):
+	ray.target_position = inputs[dir] * tile_size
+	ray.force_raycast_update()
+	if !ray.is_colliding():
+		#position += inputs[dir] * tile_size
+		var tween = get_tree().create_tween()
+		tween.tween_property(self, "position", position + inputs[dir] * tile_size, 1.0/animation_speed).set_trans(Tween.TRANS_LINEAR)
+		moving = true
+		$AnimationPlayer.play(dir)
+		await tween.finished
+		moving = false
